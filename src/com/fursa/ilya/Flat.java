68a1a5215dc8dd65e0500f1b7f197dcc87f924ee
fursailya <fursa.ilya@gmail.com>
2017-10-14 21:00:48 +0400

    Initial commit

---
+++ src/com/fursa/ilya/Flat.java
@@ -0,0 +1,41 @@
+package com.fursa.ilya;
+//lab 2 task 1
+public class Flat {
+    //площадь и количество комнат по умолчанию
+    private static final double DEFAULT_SQUARE = 50.0;
+    private static final int DEFAULT_ROOMS_COUNT = 2;
+
+    private double square; //площадь meters
+    private int room; //количество комнат
+    //конструктор по умолчанию
+    public Flat() {
+        this.room = DEFAULT_ROOMS_COUNT;
+        this.square = DEFAULT_SQUARE;
+    }
+    //конструктор может принимать площадь
+    public Flat(double square) {
+        this.room = 2;
+        this.square = square;
+    }
+    //может принимать площадь и количество комнат
+    public Flat(double square, int room) {
+        this.square = square;
+        this.room = room;
+    }
+    //получение количества комнат в квартире
+    public int getRoomCount() {
+        return room;
+    }
+    //изменение количества комнат в квартире
+    public void setRoomCount(int count) {
+        this.room = count;
+    }
+    //получение площади квартиры
+    public double getFlatSquare() {
+        return square;
+    }
+    //изменение площади
+    public void setSquare(double square) {
+        this.square = square;
+    }
+}
